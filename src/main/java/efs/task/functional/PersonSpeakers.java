4d2209d9040a2f087eacde1cbe2de2098340d879
Dawid Laska <dawidlaska888@gmail.com>
2023-05-24 22:50:46 +0200

    task streams 2 finished with passed tests

--- src/main/java/efs/task/functional/PersonSpeakers.java
+++ src/main/java/efs/task/functional/PersonSpeakers.java
@@ -1,33 +1,44 @@
 package efs.task.functional;
 
-class PersonSpeakers {
+class PersonSpeakers
+{
 
     private final String HELLO = "Hello. I'm ";
     private final String CZESC = "Czesc. Jestem ";
     private final String HALLO = "Hallo, ich heiBe ";
 
 
-    Speaker createGreetSpeaker() {
+    Speaker createGreetSpeaker()
+    {
         // Implementacja interfejsu Speaker za pomocą wyrażenia lambda. Implementacja w tej postaci możliwa jest
         // dzięki temu, że Speaker jest interfejsem funkcyjnym - posiada tylko jedną metodę abstrakcyjną.
         return p -> HELLO + p.getName();
     }
 
-    Speaker createShoutingSpeaker() {
+    Speaker createShoutingSpeaker()
+    {
         // TODO: stwórz odpowiednią implementację interfejsu Speaker, tak aby wywołanie jego metody say() powodowało
         //  zwrócenie tekstu powitania zapisanego dużymi literami. Na zasadzie:
         //      "HELLO. I'M MARIA.", gdzie Maria to imię danej osoby przekazanej w argumencie metody say().
         //  Wywołanie metody say() interfejsu odbywa się w testach metody.
         //  Aby być pewnym, że użyta jest prawidłowa pisownia zaleca się użycie odpowiedniej stałej.
-        return null;
+        return p -> (HELLO + p.getName()).toUpperCase();
     }
 
-    Speaker createGreetLocalSpeaker() {
+    Speaker createGreetLocalSpeaker()
+    {
         // TODO: stwórz odpowiednią implementację interfejsu Speaker, tak aby wywołanie jego metody say() powodowało
         //  zwrócenie tekstu powitania w odpowiednim języku. Przykładowo dla języka angielskiego:
         //       "Hello. I'm Antonina.", gdzie Antonina to imię danej osoby przekazanej w argumencie metody say().
         //  Wywołanie metody say() interfejsu odbywa się w testach metody.
         //  Aby być pewnym, że użyta jest prawidłowa pisownia zaleca się użycie odpowiednich stałych.
-        return null;
+        return p -> {
+            if (p.getCountryOfLiving() == Country.PL)
+                return CZESC + p.getName();
+            else if (p.getCountryOfLiving() == Country.USA)
+                return HELLO + p.getName();
+            else
+                return HALLO + p.getName();
+        };
     }
 }
